d31512b616991a9c893974519c38806bc2e23282
simpletoyou <3361694095@qq.om>
2022-05-05 14:29:06 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<!--
+ * @Description: 
+ * @version: 
+ * @Author: simpletoyou
+ * @Date: 2022-05-05 14:17:37
+ * @LastEditors: simpletoyou
+ * @LastEditTime: 2022-05-05 14:21:43
+-->
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
